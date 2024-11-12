efb0f2a35900d89693b757b3cf0ae0fe90cbee9d
Luis Felipe Veronezi Bernardo <luisfelipevbernardo@gmail.com>
2024-11-12 20:57:09 -0300

    muitas modificações

---
+++ js/script.js
@@ -0,0 +1,14 @@
+        // Selecionando o formulário
+        const form = document.querySelector(".contactForm")
+
+
+
+
+
+        form.addEventListener("onclick" ,(event) => {
+            event.preventDefault()
+            console.log("ola")
+      
+            alert('Mensagem enviada com sucesso!');
+
+        })
